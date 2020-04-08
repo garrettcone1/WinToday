71db1d6db98dea9b68805dcf9d633e412603c582
Garrett Cone <conegs1@gmail.com>
2020-04-07 22:53:07 -0500

    Add new files for tees.ejs, adjust sizing for home page content for mobile view.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get("/", function(req, res) {
     res.render("home");
 });
 
+// Tees Route
+router.get("/tees", function(req, res) {
+    res.render("tees");
+});
+
 module.exports = router;
